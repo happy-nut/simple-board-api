f8c5c20e748e9d1f44ef3f6270a0ac5dbcdf9ed5
Hyungsun Song <happynut.dev@gmail.com>
2021-01-13 00:51:32 +0900

    Add index.ts for CreateUserController

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,6 +1,6 @@
 import { Logger, Module } from '@nestjs/common'
 import { HealthzController } from './presentation/healthz/healthzController'
-import { CreateUserController } from './presentation/create-user/CreateUserController'
+import { CreateUserController } from './presentation/create-user'
 import { CreateUserUseCase } from './application/create-user'
 import { USER_REPOSITORY } from './domain'
 import { TypeOrmUserRepository } from './infra/typeorm'
