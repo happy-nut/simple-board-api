61c3ec2ee20b6011c79b736b1521ad1835608ddd
Hyungsun Song <happynut.dev@gmail.com>
2021-01-17 02:41:30 +0900

    Introduce createPostsOrderByCreatedAt util

---
+++ test/support/utils/createPostsOrderByCreatedAt.ts
@@ -0,0 +1,18 @@
+import { Post, PostId } from '../../../src/domain/Post'
+import { UserId } from '../../../src/domain/UserId'
+import _ from 'lodash'
+
+export function createPostsOrderByCreatedAt (count: number, userId?: UserId): Post[] {
+  return _.times(count, (i) => {
+    const time = `${i}`.padStart(2, '0')
+    return Post.create(
+      {
+        createdAt: new Date(`2021-01-16T${time}:00:00Z`),
+        content: `test-content-${i}`,
+        title: `test-title-${i}`,
+        authorId: userId ?? new UserId(`test-user-id-${i}`)
+      },
+      new PostId()
+    )
+  })
+}
