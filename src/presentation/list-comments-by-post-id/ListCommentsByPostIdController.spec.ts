0baf17e3c0e2e77216db380e929fc54a0e7c2e24
Hyungsun Song <happynut.dev@gmail.com>
2021-01-17 15:26:29 +0900

    Introduce ListCommentsByPostIdController

---
+++ src/presentation/list-comments-by-post-id/ListCommentsByPostIdController.spec.ts
@@ -0,0 +1,126 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import request from 'supertest'
+import { HttpStatus, INestApplication, Logger } from '@nestjs/common'
+import { mock, MockProxy } from 'jest-mock-extended'
+import {
+  ListCommentsByPostIdError,
+  ListCommentsByPostIdResponse,
+  ListCommentsByPostIdUseCase
+} from '../../application/list-comments-by-post-id'
+import { ListCommentsByPostIdController } from './ListCommentsByPostIdController'
+import { UserId } from '../../domain/UserId'
+import { createDummyCommentsOrderByCreatedAt } from '../../../test/support/utils'
+import { PostId } from '../../domain/PostId'
+
+describe('ListCommentsByPostIdController', () => {
+  let testingModule: TestingModule
+  let app: INestApplication
+  let uut: unknown
+  let useCase: MockProxy<ListCommentsByPostIdUseCase>
+  let logger: MockProxy<Logger>
+
+  function givenUseCaseRejectedWithPostNotFoundError () {
+    useCase.execute.mockRejectedValueOnce(ListCommentsByPostIdError.postNotFound())
+  }
+
+  function givenUseCaseRejectedWithUnknownError () {
+    useCase.execute.mockRejectedValueOnce(new Error('unknown'))
+  }
+
+  function givenUseCaseResolvedResponse (result: ListCommentsByPostIdResponse) {
+    useCase.execute.mockResolvedValueOnce(result)
+  }
+
+  beforeEach(async () => {
+    useCase = mock()
+    logger = mock()
+    testingModule = await Test
+      .createTestingModule({
+        controllers: [ListCommentsByPostIdController],
+        providers: [
+          {
+            provide: ListCommentsByPostIdUseCase,
+            useValue: useCase
+          },
+          {
+            provide: Logger,
+            useValue: logger
+          }
+        ]
+      })
+      .compile()
+    app = await testingModule
+      .createNestApplication()
+      .init()
+    uut = app.getHttpServer()
+  })
+
+  afterEach(async () => {
+    await app.close()
+    await testingModule.close()
+  })
+
+  it('responds 404 NOT_FOUND' +
+    ' when given use case rejected error with PostNotFoundError', async () => {
+    givenUseCaseRejectedWithPostNotFoundError()
+
+    const response = await request(uut)
+      .get(`/posts/${new PostId().value}/comments`)
+
+    expect(response.status).toBe(HttpStatus.NOT_FOUND)
+  })
+
+  it('responds 500 INTERNAL_SERVER_ERROR and logs' +
+    ' when given use case rejected unknown error', async () => {
+    givenUseCaseRejectedWithUnknownError()
+
+    const response = await request(uut)
+      .get(`/posts/${new PostId().value}/comments`)
+
+    expect(response.status).toBe(HttpStatus.INTERNAL_SERVER_ERROR)
+    expect(logger.error).toHaveBeenCalled()
+  })
+
+  it('responds 200 OK when given use case resolved a user', async () => {
+    const postId = new PostId()
+    const comments = createDummyCommentsOrderByCreatedAt(2, undefined, postId)
+    const useCaseResponse: ListCommentsByPostIdResponse = [
+      {
+        createdAt: comments[0].createdAt,
+        username: 'test-username',
+        content: comments[0].content,
+        id: comments[0].id.value,
+        authorId: comments[0].authorId.value
+      },
+      {
+        createdAt: comments[1].createdAt,
+        username: 'test-username',
+        content: comments[1].content,
+        id: comments[1].id.value,
+        authorId: comments[1].authorId.value
+      }
+    ]
+    givenUseCaseResolvedResponse(useCaseResponse)
+
+    const response = await request(uut)
+      .get(`/posts/${postId.value}/comments`)
+
+    expect(response.status).toBe(HttpStatus.OK)
+    expect(response.body).toEqual([
+      {
+        createdAt: useCaseResponse[0].createdAt.toISOString(),
+        authorName: useCaseResponse[0].username,
+        authorId: useCaseResponse[0].authorId,
+        content: useCaseResponse[0].content,
+        id: useCaseResponse[0].id
+      },
+      {
+        createdAt: useCaseResponse[1].createdAt.toISOString(),
+        authorName: useCaseResponse[1].username,
+        authorId: useCaseResponse[1].authorId,
+        content: useCaseResponse[1].content,
+        id: useCaseResponse[1].id
+      },
+    ])
+  })
+})
---
+++ src/presentation/list-comments-by-post-id/ListCommentsByPostIdController.ts
@@ -0,0 +1,91 @@
+import {
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiProperty
+} from '@nestjs/swagger'
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+  Param
+} from '@nestjs/common'
+import _ from 'lodash'
+import {
+  ListCommentsByPostIdError,
+  ListCommentsByPostIdUseCase
+} from '../../application/list-comments-by-post-id'
+
+interface ListCommentsByPostIdViewModelProps {
+  id: string
+  authorId: string
+  authorName: string
+  content: string
+  createdAt: Date
+}
+
+class ListCommentsByPostIdViewModel {
+  @ApiProperty({ type: String })
+  id: string
+
+  @ApiProperty({ type: String })
+  authorId: string
+
+  @ApiProperty({ type: String })
+  authorName: string
+
+  @ApiProperty({ type: String })
+  content: string
+
+  @ApiProperty({ type: Date })
+  createdAt: Date
+
+  constructor (props: ListCommentsByPostIdViewModelProps) {
+    this.id = props.id
+    this.authorName = props.authorName
+    this.authorId = props.authorId
+    this.content = props.content
+    this.createdAt = props.createdAt
+  }
+}
+
+@Controller()
+export class ListCommentsByPostIdController {
+  constructor (
+    private readonly ListCommentsByPostIdUseCase: ListCommentsByPostIdUseCase,
+    private readonly logger: Logger
+  ) {
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get('posts/:postId/comments')
+  @ApiOkResponse({ type: [ListCommentsByPostIdViewModel] })
+  @ApiNotFoundResponse({ description: 'post not found' })
+  @ApiInternalServerErrorResponse()
+  async list (@Param('postId') postId: string): Promise<ListCommentsByPostIdViewModel[]> {
+    try {
+      const responses = await this.ListCommentsByPostIdUseCase.execute({ postId })
+      return _.map(responses, (response) => ({
+        id: response.id,
+        content: response.content,
+        authorId: response.authorId,
+        authorName: response.username,
+        createdAt: response.createdAt
+      }))
+    } catch (error) {
+      if (error instanceof ListCommentsByPostIdError) {
+        switch (error.code) {
+          case 'ListCommentsByPostIdError.POST_NOT_FOUND':
+            throw new NotFoundException()
+        }
+      }
+
+      this.logger.error(error)
+      throw new InternalServerErrorException()
+    }
+  }
+}
