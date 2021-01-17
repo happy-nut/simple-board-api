84460e8bd0f0bb153fffd5399c25dd8b060b2c6d
Hyungsun Song <happynut.dev@gmail.com>
2021-01-17 13:32:54 +0900

    Use path to refer entities in database module

--- src/modules/database/DatabaseModule.ts
+++ src/modules/database/DatabaseModule.ts
@@ -2,15 +2,14 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import config from 'config'
 import { ConnectionOptions } from 'typeorm/connection/ConnectionOptions'
-import { PostEntity, UserEntity } from '../../infra/typeorm/entities'
+import path from 'path'
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...config.get<ConnectionOptions>('typeorm'),
       entities: [
-        UserEntity,
-        PostEntity
+        path.join(__dirname, '../../**/entities/*Entity.ts')
       ]
     })
   ]
